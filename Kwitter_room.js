1c862c860265db7f7bf5c31271d413a449b3cf5b
akhilshyam74 <89392108+akhilshyam74@users.noreply.github.com>
2021-11-13 12:35:06 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD4z5wOtydzcHXnguact4cfDN2ZSOF5u8w",
+    authDomain: "kwitter-homework-68cab.firebaseapp.com",
+    projectId: "kwitter-homework-68cab",
+    storageBucket: "kwitter-homework-68cab.appspot.com",
+    messagingSenderId: "234614542566",
+    appId: "1:234614542566:web:66790689bdd1665ab66b27"
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
